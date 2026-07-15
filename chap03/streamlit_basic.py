e4f1d0b23b7d803a06d6917f0891d9106d1e0fb3
coco574 <kjshyh090809@gmail.com>
2026-07-15 17:42:22 +0900

    header bar delete

--- chap03/streamlit_basic.py
+++ chap03/streamlit_basic.py
@@ -53,7 +53,7 @@ st.title("💬 Chatbot")
 
 # (1) st.session_state에 "messages"가 없으면 초기값을 설정
 if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+    st.session_state["messages"] = [{"role": "assistant", "content": "너 누구야"}]
 
 # (2) 대화 기록을 출력
 for msg in st.session_state.messages:
